c11de49f83e96339f7925771de393b0ca6382e66
Christian van Dijk <christian.vandijk@student.hu.nl>
2020-03-11 10:52:32 +0100

    fixed build

--- src/router/index.js
+++ src/router/index.js
@@ -42,6 +42,10 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/",
+    redirect: { name: 'OurFavorites' }
   }
 ];
 
